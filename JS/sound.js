48865cd4f32fc67a44d525e27ba3d23a225dfac2
Warmachine83 <romainmesguich@gmail.com>
2020-06-26 17:47:18 +0200

    Ajout bouton et changement de couleur

--- JS/sound.js
+++ JS/sound.js
@@ -1,11 +1,57 @@
 let essai = new Audio();
+let baguette = document.getElementById('baguette');
+let couleur = ["black"]
 essai.src = "SOUND/essai.mp3"
 
+function clickBtn(){
+    essai.play()
+    baguette.style.background = "green";
 
+    setTimeout(restoreColor, 2000);
+}
+
+function restoreColor(){
+    baguette.style.background = "none";
+}
 
 document.onkeypress= function sound1(e) {
     let key = event.which || event.keyCode;
     if (key == 97) {
         essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
     }
+    
+    if (key == 122) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    if (key == 101) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    if (key == 114) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    if (key == 113) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    if (key == 115) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    if (key == 100) {
+        essai.play()
+        baguette.style.background = "green";
+        setTimeout(restoreColor, 2000);
+    }
+    
+
 }
