dbbb5e47a46fb3871f5e79b2104f4d47b5fa1e35
Warmachine83 <romainmesguich@gmail.com>
2020-06-26 15:37:54 +0200

    Ajout son avec touche

--- JS/sound.js
+++ JS/sound.js
@@ -1,2 +1,11 @@
 let essai = new Audio();
 essai.src = "SOUND/essai.mp3"
+
+
+
+document.onkeypress= function sound1(e) {
+    let key = event.which || event.keyCode;
+    if (key == 97) {
+        essai.play()
+    }
+}
